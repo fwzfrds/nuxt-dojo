8754b4975cbc5f092d41ea772b7ee2c3e77d4bbc
fwzfrds <muhammadfawwaz463@gmail.com>
2023-03-29 13:35:48 +0700

    change api key to hard coded

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -3,7 +3,8 @@ export default defineEventHandler(async (e) => {
     const { code } = e.context.params
     const { currencyKey } = useRuntimeConfig()
 
-    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
+    // const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
+    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=tMj9xKjhqzzqw374spnmACaGGTeXZO8PpqkqnYAy`
 
     const { data } = await $fetch(uri)
 
